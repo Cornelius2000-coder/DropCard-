b02c56e2f79cc2681e82ff35737f4024f8d9c87f
Okenwa chidiebube <chidiebubeokenwa57@gmail.com>
2024-08-27 23:11:45 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,169 @@
+// alert("hello world")
+// prompt("Cornel")
+// confirm("do you still want to play")
+// console.log("hello miami")
+// Data Types
+
+// var name = prompt("what is your name")
+
+// // let name = prompt("what is your name")
+
+// console.log(name)
+
+// var age = 20
+
+// console.log(age)
+
+// alert("Welcome to my world");
+// prompt("What is your name");
+// prompt("What is your age");
+// prompt("What is your email");
+// prompt("What is your phone number");
+// confirm("Do you want to quit?")
+
+// let name = prompt("What is your name")
+// let age = prompt("What is your age")
+// let mail = prompt("What is your email")
+// let mobile = prompt("What is your phone number")
+// let confirm = confirm("the info is it correct")
+// // confirm("the info you inputed is correct?")
+// console.log(name)
+// console.log(age)
+// console.log(mail)
+// console.log(mobile)
+
+
+// // String
+// let firstName = "Ikenna"
+// let lastName = "John"
+// let surname = "Cornel"
+
+// // Number
+// let ag = 20
+// let amount = 2000
+// let population = 3000000
+
+// // null
+// let country = null
+
+// // Boolean
+// let active = false 
+
+// const bigInt = 1233456765434567876543456789876n;
+
+// // for backstick quotes 
+
+// let name = "cornel"
+
+// alert(`Hello ${name}`);
+
+// // for Boolean
+
+// let nameFieldCheck= true
+// let ageFieldCheck = false
+
+// let age = prompt(`How old are you?`, 100);
+
+// alert(`you are ${age} years old!`)
+
+// // for confirm
+
+// let isBoss = confirm("Are you the Boss")
+
+// alert(isBoss)
+
+// // demo 
+
+// let location = prompt("what is your daddy's name")
+// alert(location)
+
+// let firstName = "john"
+
+
+// let admin 
+// let name
+
+// name = "John"  //double quote
+// admin = name
+// alert(admin)
+
+// let myPlanet = `earth`  //backquote
+
+// let currentUser = 'obi' //single quote
+
+// const firstName = "Okenwa"
+// let surname = "Ebube"
+
+// const fullName = `Hello ${firstName} ${surname}`;
+
+// console.log(fullName)
+
+// example of asking for user information and procveessing it out 
+
+// let name = prompt('what is your name');
+// alert(`Good morning Mr ${name}`)
+// console.log(name)
+
+// alert(Boolean(2 ))
+
+// // conversion of string to Number
+
+// let firstNumber = prompt("first number....")
+// alert(typeof Number(firstNumber))
+
+// // also
+
+// let calculate = `{+firstNumber + +secondNumber}`
+
+
+// let n = 2
+
+// n *= 5 //(same as n = n * 5)
+
+// alert(n)
+
+// // Increment
+
+
+
+// let a = 1, b = 1;
+
+// let c = ++a;
+// let d = b++;
+
+// alert(c)
+// alert(d)
+
+// let e = prompt("firstnum")
+// let f = prompt("secondnum")
+// alert(+e + +f)
+// alert(`${e} ${f}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
